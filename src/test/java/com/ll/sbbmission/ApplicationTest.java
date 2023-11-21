f2d158acd5d0910d1c6c2ae0d1221fc837786983
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-21 23:49:29 +0900

    findBySubject

--- src/test/java/com/ll/sbbmission/ApplicationTest.java
+++ src/test/java/com/ll/sbbmission/ApplicationTest.java
@@ -1,4 +1,27 @@
 package com.ll.sbbmission;
 
-public class ApplicationTest {
+import com.ll.sbbmission.Entity.Question.Question;
+import com.ll.sbbmission.Repository.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void testJpa() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
+    }
 }

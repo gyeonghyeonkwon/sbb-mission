2a6cdc6fcf35a0dc7d1a24a038ec1f26d7e42e38
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-29 18:23:44 +0900

    3-14 검색 구현완료

--- src/main/java/com/ll/sbbmission/Service/QuestionService.java
+++ src/main/java/com/ll/sbbmission/Service/QuestionService.java
@@ -1,14 +1,17 @@
 package com.ll.sbbmission.Service;
 
 import com.ll.sbbmission.DataNotFoundException;
+import com.ll.sbbmission.Entity.Answer.Answer;
 import com.ll.sbbmission.Entity.Question.Question;
 import com.ll.sbbmission.Entity.user.SiteUser;
 import com.ll.sbbmission.Repository.QuestionRepository;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,10 +28,47 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
+//    private Specification<Question> search(String kw) {
+//        return new Specification<>() {
+//            private static final long serialVersionUID = 1L;
+//
+//            @Override
+//            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+//
+//                query.distinct(true);  // 중복을 제거
+//
+//                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+//
+//                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+//
+//                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+//
+//                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+//
+//                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+//
+//                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+//
+//                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+//
+//                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+//            }
+//        };
+//    }
 
-        return this.questionRepository.findAll();
+    public Page <Question> getList(int page , String kw) {
+
+        List<Sort.Order> sorts = new ArrayList<>();
+
+        sorts.add(Sort.Order.desc("createDate"));
+
+        Pageable pageable = PageRequest.of(page,10 ,Sort.by(sorts));
+
+        //Specification<Question> spec = search(kw);
+
+        return this.questionRepository.findAllByKeyword(kw , pageable);
     }
+
 
 
     public Question getQuestion(Integer id) {
@@ -58,16 +98,7 @@ public class QuestionService {
 
         this.questionRepository.save(q);
     }
-    public Page <Question> getList(int page) {
 
-        List<Sort.Order> sorts = new ArrayList<>();
-
-        sorts.add(Sort.Order.desc("createDate"));
-
-        Pageable pageable = PageRequest.of(page,10 ,Sort.by(sorts));
-
-        return this.questionRepository.findAll(pageable);
-    }
 
     public void modify(Question question , String subject , String content) {
 
@@ -88,5 +119,7 @@ public class QuestionService {
         question.getVoter().add(siteUser);
         this.questionRepository.save(question);
     }
+
+
 }
 
